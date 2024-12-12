a62e33bb449e71e5b4d308c52a2fd20a858b9392
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-13 00:06:20 +0530

    refactor: enhance PrismaClient initialization for better global access and logging

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -2,4 +2,14 @@
 
 import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;

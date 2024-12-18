515697dd8e190e29a26bd94c161f42038c9437ea
Abhik Patra <theabhik2020@gmail.com>
2024-12-19 00:36:48 +0530

    add leads schema

--- src/schemas/leads/leads.schema.ts
+++ src/schemas/leads/leads.schema.ts
@@ -1,7 +1,25 @@
 import { z } from "zod";
 
+// Social Schema
+const SocialSchema = z.object({
+  githubUrl: z.string().optional(),
+  instagramUrl: z.string().optional(),
+  email: z.string().email(),
+  portfolioUrl: z.string().optional(),
+  linkedinUrl: z.string().optional(),
+});
 
+// Leads Schema
+const LeadsSchema = z.object({
+  fullName: z.string().min(4).max(15),
+  isCoreMember: z.boolean(),
+  coreMemberPosition: z.string().optional(),
+  isCurrent: z.boolean(),
+  profilePhoto: z.string(),
+  domainGroupId: z.string(),
+  domainNameId: z.string(),
+  index: z.number(),
+});
 
-export const leadSchema = z.object({
-    
-})
+// Export schemas
+export { LeadsSchema, SocialSchema };

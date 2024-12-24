5c52c6edd02024393d5d173247ab2873cf9b0822
Abhik Patra <theabhik2020@gmail.com>
2024-12-24 23:06:22 +0530

    feat: add navigation to add event page in EventHeader component

--- src/components/(admin)/events/EventHeader.tsx
+++ src/components/(admin)/events/EventHeader.tsx
@@ -1,11 +1,13 @@
-import { Button } from '@/components/ui/button'
+"use client";
 import React from 'react'
-
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation';
 function EventHeader() {
+    const router = useRouter();
     return (
         <div className="flex justify-between items-center">
             <h1 className="text-2xl font-bold">Events</h1>
-            <Button>
+            <Button  onClick={()=>router.push("/events/add")} >
                 Add Event
             </Button>
         </div>

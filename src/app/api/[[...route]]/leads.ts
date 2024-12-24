78bffbdb020ff415ad29159c8e0d611aad372cfb
Abhik Patra <theabhik2020@gmail.com>
2024-12-24 22:00:21 +0530

    refactor: restore token validation and lead handling logic in leads API for improved functionality

--- src/app/api/[[...route]]/leads.ts
+++ src/app/api/[[...route]]/leads.ts
@@ -92,58 +92,58 @@ const leads = new Hono()
   })
   .post("/update-lead/:id", zValidator("json", LeadsSchema), async (c) => {
     try {
-      // const token = getCookie(c, "token");
-      // if (!token) {
-      //   return c.json({ success: false, error: "Token not found" }, 401);
-      // } else {
-      //   const userToken = decodeSignInToken(token);
-      //   const { id } = userToken.payload;
-      //   const leadId = c.req.param("id");
-      //   const {
-      //     fullName,
-      //     isCoreMember,
-      //     coreMemberPositionId,
-      //     isCurrent,
-      //     profilePhoto,
-      //     domainGroupId,
-      //     domainNameId,
-      //     index,
-      //     Social,
-      //   } = c.req.valid("json");
-      //   const { githubUrl, instagramUrl, linkedinUrl, email, portfolioUrl } =
-      //     Social;
-      //   const lead = await db.leads.update({
-      //     where: { id: leadId },
-      //     data: {
-      //       fullName,
-      //       isCoreMember,
-      //       coreMemberPositionId,
-      //       isCurrent,
-      //       profilePhoto,
-      //       domainGroupId,
-      //       domainNameId,
-      //       userId: id,
-      //       index: parseInt(index!),
-      //     },
-      //   });
-      //   await db.social.create({
-      //     data: {
-      //       leadId: lead.id,
-      //       githubUrl,
-      //       instagramUrl,
-      //       linkedinUrl,
-      //       email,
-      //       portfolioUrl,
-      //     },
-      //   });
-      //   return c.json(
-      //     {
-      //       success: true,
-      //       message: "Leads Updates Success",
-      //     },
-      //     200
-      //   );
-      // }
+      const token = getCookie(c, "token");
+      if (!token) {
+        return c.json({ success: false, error: "Token not found" }, 401);
+      } else {
+        const userToken = decodeSignInToken(token);
+        const { id } = userToken.payload;
+        const leadId = c.req.param("id");
+        const {
+          fullName,
+          isCoreMember,
+          coreMemberPositionId,
+          isCurrent,
+          profilePhoto,
+          domainGroupId,
+          domainNameId,
+          index,
+          Social,
+        } = c.req.valid("json");
+        const { githubUrl, instagramUrl, linkedinUrl, email, portfolioUrl } =
+          Social;
+        const lead = await db.leads.update({
+          where: { id: leadId },
+          data: {
+            fullName,
+            isCoreMember,
+            coreMemberPositionId,
+            isCurrent,
+            profilePhoto,
+            domainGroupId,
+            domainNameId,
+            userId: id,
+            index: parseInt(index!),
+          },
+        });
+        await db.social.create({
+          data: {
+            leadId: lead.id,
+            githubUrl,
+            instagramUrl,
+            linkedinUrl,
+            email,
+            portfolioUrl,
+          },
+        });
+        return c.json(
+          {
+            success: true,
+            message: "Leads Updates Success",
+          },
+          200
+        );
+      }
     } catch (error) {
       console.error("Sign-in error:", error);
       return c.json(
@@ -157,46 +157,46 @@ const leads = new Hono()
   })
   .get("/:id", async (c) => {
     try {
-      // const token = getCookie(c, "token");
-      // if (!token) {
-      //   return c.json({ success: false, error: "Token not found" }, 401);
-      // } else {
-      //   const lead = await db.leads.findFirst({
-      //     where: {
-      //       id: c.req.param("id"),
-      //     },
-      //     include: {
-      //       Social: true,
-      //       coreMemberPosition: {
-      //         select: {
-      //           title: true,
-      //         },
-      //       },
-      //       createdBy: {
-      //         select: {
-      //           firstName: true,
-      //         },
-      //       },
-      //       domainGroup: {
-      //         select: {
-      //           title: true,
-      //         },
-      //       },
-      //       domainName: {
-      //         select: {
-      //           title: true,
-      //         },
-      //       },
-      //     },
-      //   });
-      //   return c.json(
-      //     {
-      //       success: true,
-      //       lead: lead,
-      //     },
-      //     200
-      //   );
-      // }
+      const token = getCookie(c, "token");
+      if (!token) {
+        return c.json({ success: false, error: "Token not found" }, 401);
+      } else {
+        const lead = await db.leads.findFirst({
+          where: {
+            id: c.req.param("id"),
+          },
+          include: {
+            Social: true,
+            coreMemberPosition: {
+              select: {
+                title: true,
+              },
+            },
+            createdBy: {
+              select: {
+                firstName: true,
+              },
+            },
+            domainGroup: {
+              select: {
+                title: true,
+              },
+            },
+            domainName: {
+              select: {
+                title: true,
+              },
+            },
+          },
+        });
+        return c.json(
+          {
+            success: true,
+            lead: lead,
+          },
+          200
+        );
+      }
     } catch (error) {
       console.error("Sign-in error:", error);
       return c.json(
@@ -255,37 +255,37 @@ const leads = new Hono()
     zValidator("json", LeadsSchema.pick({ isCurrent: true })),
     async (c) => {
       try {
-        // const token = getCookie(c, "token");
-        // if (!token) {
-        //   return c.json({ success: false, error: "Token not found" }, 401);
-        // } else {
-        //   const { isCurrent } = c.req.valid("json");
-        //   const id = c.req.param("id");
-        //   // const lead = await db.leads.findFirst({
-        //   //   where: {
-        //   //     id: c.req.param("id"),
-        //   //   },
-        //   // });
-        //   // console.log(lead);
-        //   // await db.leads.delete({
-        //   //   where: { id },
-        //   // });
-        //   await db.leads.update({
-        //     where: {
-        //       id: id,
-        //     },
-        //     data: {
-        //       isCurrent,
-        //     },
-        //   });
-        //   return c.json(
-        //     {
-        //       success: true,
-        //       message: "Successfully Modified",
-        //     },
-        //     200
-        //   );
-        // }
+        const token = getCookie(c, "token");
+        if (!token) {
+          return c.json({ success: false, error: "Token not found" }, 401);
+        } else {
+          const { isCurrent } = c.req.valid("json");
+          const id = c.req.param("id");
+          // const lead = await db.leads.findFirst({
+          //   where: {
+          //     id: c.req.param("id"),
+          //   },
+          // });
+          // console.log(lead);
+          // await db.leads.delete({
+          //   where: { id },
+          // });
+          await db.leads.update({
+            where: {
+              id: id,
+            },
+            data: {
+              isCurrent,
+            },
+          });
+          return c.json(
+            {
+              success: true,
+              message: "Successfully Modified",
+            },
+            200
+          );
+        }
       } catch (error) {
         console.log("Sign-in error:", error);
         return c.json(
@@ -300,24 +300,24 @@ const leads = new Hono()
   )
   .get("/delete-lead/:id", async (c) => {
     try {
-      // const token = getCookie(c, "token");
-      // if (!token) {
-      //   return c.json({ success: false, error: "Token not found" }, 401);
-      // } else {
-      //   const leadId = c.req.param("id");
-      //   const deletedLead = await db.leads.delete({
-      //     where: { id: leadId },
-      //   });
+      const token = getCookie(c, "token");
+      if (!token) {
+        return c.json({ success: false, error: "Token not found" }, 401);
+      } else {
+        const leadId = c.req.param("id");
+        const deletedLead = await db.leads.delete({
+          where: { id: leadId },
+        });
 
-      //   console.log("Lead deleted:", deletedLead);
-      //   return c.json(
-      //     {
-      //       success: true,
-      //       lead: deletedLead,
-      //     },
-      //     200
-      //   );
-      // }
+        console.log("Lead deleted:", deletedLead);
+        return c.json(
+          {
+            success: true,
+            lead: deletedLead,
+          },
+          200
+        );
+      }
     } catch (error) {
       console.error("Sign-in error:", error);
       return c.json(

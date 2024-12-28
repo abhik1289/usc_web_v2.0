2e52d65579a6caff16a4711f423530d32e8349b9
Abhik Patra <theabhik2020@gmail.com>
2024-12-29 02:37:45 +0530

    refactor: remove unused table imports in TableBodyComponents

--- src/components/(admin)/events/TableBodyCompenents.tsx
+++ src/components/(admin)/events/TableBodyCompenents.tsx
@@ -1,10 +1,6 @@
 import { Button } from "@/components/ui/button";
 import {
-    Table,
-    TableBody,
     TableCell,
-    TableHead,
-    TableHeader,
     TableRow,
 } from "@/components/ui/table";
 

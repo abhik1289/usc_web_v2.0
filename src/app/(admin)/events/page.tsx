35834c5b0f73c3792e270b1cce3cf50d2b54c27b
Abhik Patra <theabhik2020@gmail.com>
2024-12-29 02:50:33 +0530

    refactor: remove unused import and interface in events page component

--- src/app/(admin)/events/page.tsx
+++ src/app/(admin)/events/page.tsx
@@ -1,26 +1,13 @@
 "use client";
 
 import React from "react";
-import axios from "axios";
+
 
 import EventTable from "@/components/(admin)/events/even-table";
 import EventHeader from "@/components/(admin)/events/EventHeader";
 
 
-interface Event {
-  id: number;
-  title: string;
-  location: string;
-  date: any;
-  displayType: boolean;
-  eventType: string;
-  startTime1: string;
-  startTime2?: string;
-  endTime1: string;
-  endTime2?: string;
-  startDate: string;
-  endDate?: any;
-}
+
 
 
 

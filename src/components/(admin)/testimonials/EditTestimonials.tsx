cca9a3067183230af1c287a264c2455a2d2860d2
Abhik Patra <theabhik2020@gmail.com>
2024-12-29 02:40:49 +0530

    refactor: clean up imports and remove unused state in EditTestimonials component

--- src/components/(admin)/testimonials/EditTestimonials.tsx
+++ src/components/(admin)/testimonials/EditTestimonials.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
+import React from "react";
 import {
   Card,
   CardContent,
@@ -15,19 +14,19 @@ import useGetTestimonial from "@/hooks/api/testimonials/useGetTestimonial";
 export const EditTestimonial = () => {
   const param = useSearchParams();
   // const [data, setData] = useState();
-  const id = param.get("id")||"";
- 
-  const { data,isLoading } = useGetTestimonial(id);
+  const id = param.get("id") || "";
+
+  const { data } = useGetTestimonial(id);
 
   console.log(data);
   return (
     <div className="p-4">
       <Card className="">
         <CardHeader>
-        <CardTitle>Update Testimonial</CardTitle>
-<CardDescription>
-Refine your experience by updating a testimonial to inspire and inform others.
-</CardDescription>
+          <CardTitle>Update Testimonial</CardTitle>
+          <CardDescription>
+            Refine your experience by updating a testimonial to inspire and inform others.
+          </CardDescription>
         </CardHeader>
         <CardContent>
           {data && <AddTestimonialsForm

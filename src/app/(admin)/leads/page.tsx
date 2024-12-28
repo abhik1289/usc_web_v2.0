e84e4cab07dd1dd8f5386439cca9905c559b63cf
Abhik Patra <theabhik2020@gmail.com>
2024-12-29 02:51:19 +0530

    refactor: remove unused imports and interface in leads page component

--- src/app/(admin)/leads/page.tsx
+++ src/app/(admin)/leads/page.tsx
@@ -1,36 +1,11 @@
 "use client";
 
-import React, { useState } from "react";
-import LeadsDialog from "@/components/(admin)/leads/LeadsDialog";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useGetLeads } from "@/hooks/api/leads/useGetLeads";
+import React from "react";
+
 import LeadsTable from "@/components/(admin)/leads/LeadsTable";
 import LeadsFilterBox from "@/components/(admin)/leads/LeadsFilterBox";
 
-interface Lead {
-  id: number;
-  name: string;
-  domainType: string;
-  domainName: string;
-  socials: string;
-  photo: File | null;
-}
+
 
 export default function LeadsPage() {
 

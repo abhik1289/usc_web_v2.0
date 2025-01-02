3120f327ac236f8dd6b6ddbd5ff52326648f1a12
Abhik Patra <theabhik2020@gmail.com>
2025-01-02 10:22:54 +0530

    feat: implement logout confirmation dialog in ProfileHeader component

--- src/components/(admin)/profile/profile-header.tsx
+++ src/components/(admin)/profile/profile-header.tsx
@@ -1,5 +1,10 @@
+"use router"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import AlertDialogBox from "../AlertDialog.tsx/AlertDialog";
+import { useState } from "react";
+import useLogout from "@/hooks/api/profile/useLogout";
+import { useRouter } from "next/navigation";
 
 
 interface ProfileData {
@@ -19,6 +24,15 @@ export default function ProfileHeader({
   createdAt,
   role
 }: ProfileData) {
+
+  //states
+  const [show, setShow] = useState(false);
+
+  //hooks
+
+  const logout = useLogout();
+  const router = useRouter();
+
   // This data would typically come from your database or API
   const user = {
     name: `${first_name} ${last_name}`,
@@ -27,10 +41,20 @@ export default function ProfileHeader({
     joinDate: `${createdAt}`,
     profilePhoto: `${profile_photo}`,
   };
-  
 
-  const handleLogout =()=>{
-    console.log("Logout")
+
+  const handleLogout = () => {
+    setShow(true);
+  }
+
+  const handleLogoutConfirm = () => {
+    logout.mutate(undefined, {
+      onSuccess: () => {
+        // Redirect to the sign-in page after successful logout
+        router.push("/sign-in");
+        setShow(false);
+      }
+    })
   }
 
   return (
@@ -50,6 +74,13 @@ export default function ProfileHeader({
       <Button onClick={handleLogout} className="absolute top-6 right-6">
         Logout
       </Button>
+      <AlertDialogBox
+        title="Confirm Logout"
+        description="Are you sure you want to log out? Any unsaved changes will be lost."
+        show={show}
+        setShow={() => setShow(false)}
+        onConfirm={handleLogoutConfirm}
+      />
     </div>
   );
 }

6bca0f492fb383450958bbbd008b9a6cb2897858
Abhik Patra <theabhik2020@gmail.com>
2025-01-04 01:28:46 +0530

    feat: add AdvisorBox component with advisor listing and actions

--- src/components/(admin)/advisor/AdvisorBox.tsx
+++ src/components/(admin)/advisor/AdvisorBox.tsx
@@ -1,7 +1,11 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import useGetAdvisor from '@/hooks/api/mentor/useGetAdvisor';
 import React from 'react'
-
+import Image from 'next/image';
 function AdvisorBox() {
+    const advisors = useGetAdvisor();
+    console.log(advisors.data)
     return (
         <Card>
             <CardHeader>
@@ -13,9 +17,41 @@ function AdvisorBox() {
                 </CardDescription>
             </CardHeader>
             <CardContent>
-                A
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="">Id</TableHead>
+                            <TableHead>Fullname</TableHead>
+                            <TableHead>School</TableHead>
+                            <TableHead>Photo</TableHead>
+                            <TableHead>Action</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+
+                        {
+                            advisors.data?.map((advisor: any, i: number) => (
+                                <TableRow key={i}>
+                                    <TableCell>{i + 1}</TableCell>
+                                    <TableCell>{advisor.fullName}</TableCell>
+                                    <TableCell>{advisor.school}</TableCell>
+                                    <TableCell>
+                                        <div className="img_container rounded-md overflow-hidden w-[50px] h-[50px]">
+                                            <Image src={advisor.profilePhoto} width={100} height={100} alt='' />
+                                        </div>
+                                    </TableCell>
+                                    <TableCell>
+                                        <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                                        <button className="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        }
+
+                    </TableBody>
+                </Table>
             </CardContent>
-        </Card>
+        </Card >
     )
 }
 
--- src/hooks/api/mentor/useGetAdvisor.ts
+++ src/hooks/api/mentor/useGetAdvisor.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import React from 'react'
 
-export default function useGetMentor() {
+export default function useGetAdvisor() {
 
     const query = useQuery({
         queryKey: ["leads"],

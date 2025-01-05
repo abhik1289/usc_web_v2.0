cb420b91d4e0ddabe05570c21f4900c29802122a
Abhik Patra <theabhik2020@gmail.com>
2025-01-05 20:23:12 +0530

    feat: add error handling for event retrieval in EventForm and improve conditional rendering

--- src/components/(admin)/events/add-event.tsx
+++ src/components/(admin)/events/add-event.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import AddEventFrom from "./AddEventFrom";
 import { useSearchParams } from "next/navigation";
 import useGetEventById from "@/hooks/api/events/useGetEventById";
@@ -13,10 +13,19 @@ export default function EventForm() {
   const id = params.get("id");
 
   const event = useGetEventById(id);
-  console.log(event.data)
+  console.log(event)
   //Dynamic event title and description
   const title = id ? `Edit Event: ${event.data && event.data.title}` : 'Add New Event';
   const description = id ? `Edit the details for event "${event.data && event.data.title}"` : 'Create a new event';
+
+
+  if (id && event.data === null) {
+    return <div className=" w-[350px] italic">
+      <p>
+        No event is found. Invalid Id
+      </p>
+    </div>
+  }
 
 
 
@@ -28,52 +37,53 @@ export default function EventForm() {
           {description}
         </CardDescription>
       </CardHeader>
+
       {
 
-      }
-      {id && event.data ? <EditEventFrom
-        defaultValues={{
-          title: event.data && event.data.title,
-          description: "",
-          location: "",
-          linkedinUrl: "",
-          instagramUrl: "",
-          isPublic: false,
-          duration: 'SINGLE',
-          startDate: "",
-          startTime: "",
-          endTime: "",
-          startTime1: "",
-          endTime1: "",
-          startTime2: "",
-          endTime2: "",
-          startDate1: "",
-          endDate1: "",
-          startDate2: "",
-          endDate2: "",
-        }}
-        disable={event.isLoading}
-      /> :
-        <AddEventFrom defaultValues={{
-          title: "",
-          description: "",
-          location: "",
-          linkedinUrl: "",
-          instagramUrl: "",
-          isPublic: false,
-          duration: 'SINGLE',
-          startDate: "",
-          startTime: "",
-          endTime: "",
-          startTime1: "",
-          endTime1: "",
-          startTime2: "",
-          endTime2: "",
-          startDate1: "",
-          endDate1: "",
-          startDate2: "",
-          endDate2: "",
-        }} />}
+        id && event.data ? <EditEventFrom
+          defaultValues={{
+            title: event.data && event.data.title,
+            description: "",
+            location: "",
+            linkedinUrl: "",
+            instagramUrl: "",
+            isPublic: false,
+            duration: 'SINGLE',
+            startDate: "",
+            startTime: "",
+            endTime: "",
+            startTime1: "",
+            endTime1: "",
+            startTime2: "",
+            endTime2: "",
+            startDate1: "",
+            endDate1: "",
+            startDate2: "",
+            endDate2: "",
+          }}
+          disable={event.isLoading}
+        /> :
+          <AddEventFrom defaultValues={{
+            title: "",
+            description: "",
+            location: "",
+            linkedinUrl: "",
+            instagramUrl: "",
+            isPublic: false,
+            duration: 'SINGLE',
+            startDate: "",
+            startTime: "",
+            endTime: "",
+            startTime1: "",
+            endTime1: "",
+            startTime2: "",
+            endTime2: "",
+            startDate1: "",
+            endDate1: "",
+            startDate2: "",
+            endDate2: "",
+          }} />}
     </Card>
   );
+
 }

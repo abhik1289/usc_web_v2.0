93826c4e9815bdfcdc19bde84a6399a3c9773993
Abhik <theabhik2020@gmail.com>
2025-01-15 01:24:30 +0530

    feat: remove unused icon imports from dashboard layout

--- src/app/context/dashBoardLayout.tsx
+++ src/app/context/dashBoardLayout.tsx
@@ -4,17 +4,8 @@ import {
   SidebarInset,
   SidebarProvider,
 } from "@/components/ui/sidebar";
-import { MdEventAvailable } from "react-icons/md";
-import { FaUsers } from "react-icons/fa";
-import { RiUserShared2Fill } from "react-icons/ri";
-import { SiGoogledomains } from "react-icons/si";
 import DashBoardHeader from "@/components/(admin)/header/dashboard-header";
-import { GiChampions } from "react-icons/gi";
-import { VscFeedback } from "react-icons/vsc";
-import { FaUserTie } from "react-icons/fa";
-import { FaUserLarge } from "react-icons/fa6";
-import { SiCloudflareworkers } from "react-icons/si";
-import useHomeStore from "@/store/HomeDetails";
+
 // export const 
 
 export default function DashBoardLayout({

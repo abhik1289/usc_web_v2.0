cf6bbaa3d9cd0a4e00c769bd0014c32992319ffe
Abhik <theabhik2020@gmail.com>
2025-01-15 01:24:15 +0530

    feat: refactor dashboard card list to use dynamic data from HomeDetails store

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,16 +1,75 @@
 "use client"
 
 import React from "react";
-import { dashboardCArdList } from "@/app/context/dashBoardLayout";
+// import { dashboardCArdList } from "@/app/context/dashBoardLayout";
 
 import useAuthStore from "@/store/Auth";
 import { useTheme } from "next-themes";
 import { useBasic } from "@/hooks/api/home/getBasic";
-
+import { MdEventAvailable } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import { RiUserShared2Fill } from "react-icons/ri";
+import { SiGoogledomains } from "react-icons/si";
+import { GiChampions } from "react-icons/gi";
+import { VscFeedback } from "react-icons/vsc";
+import { FaUserTie } from "react-icons/fa";
+import { FaUserLarge } from "react-icons/fa6";
+import { SiCloudflareworkers } from "react-icons/si";
+import useHomeStore from "@/store/HomeDetails";
 function HomePage() {
 
-   
+  const { event, champion, currentLead, textnimial, domain, advisor, formertLead, mentor } = useHomeStore();
 
+  const dashboardCArdList = [
+    {
+      id: 1,
+      number: currentLead,
+      title: "Current Leads",
+      icon: <FaUsers />,
+    },
+    {
+      id: 2,
+      number: formertLead,
+      title: "Former Leads",
+      icon: <RiUserShared2Fill />,
+    },
+    {
+      id: 3,
+      number: event,
+      title: "Events",
+      icon: <MdEventAvailable />,
+    },
+    {
+      id: 4,
+      number: domain,
+      title: "Domains",
+      icon: <SiCloudflareworkers />,
+    },
+    {
+      id: 5,
+      number: advisor,
+      title: "Advisors",
+      icon: <FaUserLarge />,
+    },
+    {
+      id: 6,
+      number: mentor,
+      title: "Mentors",
+      icon: <FaUserTie />,
+    },
+    {
+      id: 7,
+      number: textnimial,
+      title: "Testimonials",
+      icon: <VscFeedback />,
+    },
+    {
+      id: 8,
+      number: champion,
+      title: "Champions",
+      icon: <GiChampions />,
+    },
+  ];
 
 
 
--- src/app/context/dashBoardLayout.tsx
+++ src/app/context/dashBoardLayout.tsx
@@ -14,62 +14,18 @@ import { VscFeedback } from "react-icons/vsc";
 import { FaUserTie } from "react-icons/fa";
 import { FaUserLarge } from "react-icons/fa6";
 import { SiCloudflareworkers } from "react-icons/si";
-export const dashboardCArdList = [
-  {
-    id: 1,
-    number: 15,
-    title: "Current Leads",
-    icon: <FaUsers />,
-  },
-  {
-    id: 2,
-    number: 8,
-    title: "Former Leads",
-    icon: <RiUserShared2Fill />,
-  },
-  {
-    id: 3,
-    number: 5,
-    title: "Events",
-    icon: <MdEventAvailable />,
-  },
-  {
-    id: 4,
-    number: 9,
-    title: "Domains",
-    icon: <SiCloudflareworkers />,
-  },
-  {
-    id: 5,
-    number: 9,
-    title: "Advisors",
-    icon: <FaUserLarge />,
-  },
-  {
-    id: 6,
-    number: 9,
-    title: "Mentors",
-    icon: <FaUserTie />,
-  },
-  {
-    id: 7,
-    number: 9,
-    title: "Testimonials",
-    icon: <VscFeedback />,
-  },
-  {
-    id: 8,
-    number: 9,
-    title: "Champions",
-    icon: <GiChampions />,
-  },
-];
+import useHomeStore from "@/store/HomeDetails";
+// export const 
 
 export default function DashBoardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+
+  
+
   return (
     <SidebarProvider>
       <AppSidebar />
